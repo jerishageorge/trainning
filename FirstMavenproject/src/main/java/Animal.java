3dd0b3515a65882823298252c4d16d6f1cd355c6
jerishageorge <mv8261@gmail.com>
2023-06-22 19:46:32 +0400

    project created

---
+++ FirstMavenproject/src/main/java/Animal.java
@@ -0,0 +1,14 @@
+
+public class Animal
+{
+String color="white";
+ public void eat()
+{
+System.out.println("eating");
+
+}
+ public void Animal()
+ {
+	 
+ }
+}
---
+++ FirstMavenproject/src/main/java/Dog.java
@@ -0,0 +1,11 @@
+
+public class Dog extends Animal
+{
+String name="black";
+	public void display()
+	{
+		System.out.println("display");
+
+	}
+
+}
